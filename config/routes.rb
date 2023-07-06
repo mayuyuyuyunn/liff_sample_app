8f280323a7c6c0fac2e143e8e84f8ed8ee2f86b8
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2023-07-06 22:14:58 +0900

    Add: Userにnewとcreateのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resource :user, only: %i[new create]
 end
