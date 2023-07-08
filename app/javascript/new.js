eebb57eb8ed610fa949ac02919a7ce2b048ffc9a
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2023-07-08 22:10:55 +0900

    Add: new.jsの内容を追加

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -1,1 +1,35 @@
-console.log('new');
+document.addEventListener('DOMContentLoaded', () => {
+  // csrf-tokenを取得
+  const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+  const LIFF_ID = "自身のLIFF_IDをコピペする";
+
+  liff
+  .init({
+      liffId: LIFF_ID,
+      // 他のブラウザで開いたときは初期化と一緒にログインもさせる
+      withLoginOnExternalBrowser: true
+  })
+
+  liff
+  .ready.then(() => {
+      const idToken = liff.getIDToken()
+      const body = `idToken=${idToken}`
+      const request = new Request('/user', {
+          headers: {
+              'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+              'X-CSRF-Token': token
+          },
+          method: 'POST',
+          body: body
+      });
+
+      fetch(request)
+      .then(response => response.json())
+      .then(data => {
+          data_id = data
+      })
+      .then(() => {
+          window.location = '/after_login'
+      })
+  })
+})
